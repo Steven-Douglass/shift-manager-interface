86fc2e2888a2bf39c932495f6f597f0803afdd6c
root <root@ip-172-31-35-155.ec2.internal>
2020-04-02 18:37:38 +0000

    Initial commit

---
+++ navbar.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="./styles.css">
+  </head>
+  <body>
+    <header>
+      <div id="navbar">
+        <p>View Schedule</p>
+	<p>Settings</p>
+	<p><a href='./index.html'>Logout</a></p>
+      </div>
+  </header>
+  </body>
+</html> 
